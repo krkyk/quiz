b5594920606da48215135b510f65ec322cc29ab7
krkyk <madeinpink1111@gmail.com>
2022-08-28 08:32:28 +0000

    [Add]ヘッダー/フッター

--- app/controllers/public/primes_controller.rb
+++ app/controllers/public/primes_controller.rb
@@ -4,7 +4,13 @@ class Public::PrimesController < ApplicationController
     @prime = Prime.new
   end
 
+  def create
+    @prime = Prime.new(prime_params)
+    @prime.save
+  end
+
   def answer
+    @prime = Prime.find(params[:id])
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
 
 scope module: :public do
   root to: 'homes#top'
+  get 'about' => 'homes#about'
   resources :primes, only:[:new, :create]
   get 'primes/:id/answers' => 'primes#answers', as: :answer
 end
